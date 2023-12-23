e44a2974b64324feec74f8a6f46168f24c41d724
Leonardo Godoy <leonardo.godoy.souza@gmail.com>
2023-12-23 17:46:43 -0300

    Add uninstall script

---
+++ uninstall.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if ! command -v rcdn &>/dev/null
+then
+  echo 'Installing rcm to manage dotfiles'
+  echo 'you can find more at https://github.com/thoughtbot/rcm'
+  brew install rcm
+  echo
+fi
+
+echo 'Uninstalling dotfiles'
+
+rcdn -v -d ~/dotfiles -x install.sh -x README
+
